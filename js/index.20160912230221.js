44d22ea6a382fe64a47272017928743a4706c3a4
yzwal566 <bryanyang@yeah.net>
2016-09-12 23:05:53 +0800

    init
    初始化

---
+++ js/index.20160912230221.js
@@ -0,0 +1,9 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/zhan-tie-tu-xiang-1920.png' : 'images/zhan-tie-tu-xiang-960.png');};
+if(!window.HTMLPictureElement){r();}
+
+});
